c4eaa51955d0f7b78da5f36c9c95362128f9ffbf
alexiotovv <alexiotovv@gmail.com>
2023-02-06 18:14:49 -0500

    clase3js

---
+++ jsFunciones/Funciones.js
@@ -0,0 +1,11 @@
+//entendiendo funciones
+function EncontrarPromedio(n1,n2,n3,n4){
+    //aqui va el codigo
+    suma=0;
+    suma=(parseInt(n1)+parseInt(n2)+parseInt(n3)+parseInt(n4))/4;
+    return (suma);
+}
+
+function parseLocoMensajito(nombre){
+    return('Hola ' + nombre);
+}
